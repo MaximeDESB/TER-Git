c5e0185928ab57cb2ccfda354c7ece0790825e79
MaximeDESBOIS <max7410@hotmail.fr>
2018-03-13 19:17:04 +0100

    selectionner sur master

--- myscript.js
+++ myscript.js
@@ -8,92 +8,157 @@ var svg = d3.select("body")
 var width = svg.attr("width");
 var height = svg.attr("height");
    
-
 //---------------------------------------------------------------------------------------
 
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("link", d3.forceLink().id(function(d) { return d.node; }))
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2));
+    
 
+//----------------------------------------variables-------------------------------------
 
+var nodes;
+var edges;
 
-//-----------------------------------------tableau dynamique-------------------------------
-
-    /*  d3.csv("data.csv", function(data) {
-		
-		console.log(data);
-            var container = d3.select("body")
-                .append("table")
-
-                .selectAll("tr")
-                .data(data).enter()
-                .append("tr")
-
-                .selectAll("td")
-               	.data(function(d) { return Object.values(d); })
-		.enter()
-                .append("td")
-                .text(function(d) { return d; });
-});*/
-
-
-	d3.json("data.json", function(data) {
-		
-		console.log(data);
-            var container = d3.select("body")
-
-                .append("table")
-                .selectAll("tr")
-                .data(data.nodes)
-                .enter()
-                .append("tr")
-
-                .selectAll("td")
-               	.data(function(d) { return Object.values(d); })
-								.enter()
-                .append("td")
-                .text(function(d) { return d; });
-                
-  //-------------------------------------link------------------------------------
-                
-                 var link = svg.append("g")
-      .attr("class", "links")
-    .selectAll("line")
-    .data(data.links)
-    .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+							
+//----------------------------------------extraction données------------------------------
+	d3.csv("nodes.csv", function(data1) {
+	    d3.csv("edges.csv", function(data2) {
 	
+		nodes = data1;
+		edges = data2;
+		
+		//tableau nodes
+        
+        var div1 = d3.select("body").append("div");
+	        var table = div1.append("table");
+	        var thead = table.append('thead');
+	        var tbody = table.append('tbody');
+
+	        thead.append('tr')
+	             .selectAll('th')
+	             .data(nodes.columns)
+	             .enter()
+	             .append('th')
+	             .text(function (d) { return d });
+	             
+	             
+	        var rows_nodes = tbody.selectAll('tr')
+	                        .data(nodes)
+	                        .enter()
+	                        .append('tr')
+	                        .attr("id", function(d) { return "row_node" + d.node;});
+	                        
+
+	        var cells = rows_nodes.selectAll('td')
+	                        .data(function(row) {
+	    	                        return nodes.columns.map(function (column) {
+	    		                                            return { column: column, value: row[column] }
+	                                           })
+                                     })
+                            .enter()
+                            .append('td')
+                            .text(function (d) { return d.value });
+                            
+            table.attr("class", "table");
+            cells.attr("class", "cells");
+            thead.attr("class", "thead");
+            rows_nodes.attr("class", "rows");
+            div1.attr("id", "div1");
+            
+          
+            
+       //tableau edges 
+       
+       var div2 = d3.select("body").append("div");
+	        var table = div2.append("table");
+	        var thead = table.append('thead');
+	        var tbody = table.append('tbody');
+
+	        thead.append('tr')
+	             .selectAll('th')
+	             .data(edges.columns)
+	             .enter()
+	             .append('th')
+	             .text(function (d) { return d });
+	             
+	             
+	        var rows_links = tbody.selectAll('tr')
+	                        .data(edges)
+	                        .enter()
+	                        .append('tr')
+	                        .attr("id" , function(d) {return "row_link" + d.source + d.target;});
+
+	        var cells = rows_links.selectAll('td')
+	                        .data(function(row) {
+	    	                        return edges.columns.map(function (column) {
+	    		                                            return { column: column, value: row[column] }
+	                                           })
+                                     })
+                            .enter()
+                            .append('td')
+                            .text(function (d) { return d.value });
+                            
+            table.attr("class", "table");
+            cells.attr("class", "cells");
+            rows_links.attr("class", "rows");
+            div2.attr("id", "div2");
+            thead.attr("class", "thead");
+            
+            
+       
+        
+        
+        //-------------------------------------link------------------------------------
+                
+       var link = svg.append("g")
+                     .attr("class", "links")
+                     .selectAll("line")
+                     .data(edges)
+                     .enter().append("line")
+                     .attr("id" , function(d) {return "link" + d.source + d.target;});
+                     
+
 	
+
 //----------------------------------------node-----------------------------------
 
- var node = svg.append("g")
-      .attr("class", "nodes")
-    .selectAll("circle")
-    .data(data.nodes)
-    .enter().append("circle")
-      .attr("r", 5)
-      .attr("fill", function(d) { return color(d.group); })
-      .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
+
+
+        var node = svg.append("g")
+                      .attr("class", "nodes")
+                      .selectAll("circle")
+                      .data(nodes)
+                      .enter().append("circle")
+                      .attr("r", 5)
+                      .attr("id" , function(d) {return "node"+ d.node;})
+                      .call(d3.drag()
+                      .on("start", dragstarted)
+                      .on("drag", dragged)
+                      .on("end", dragended));
+
           
-//--------------------------------------------------------------------------------------------
+
+//------------------------------------liaison node/edges--------------------------------------------------------
+
 
  node.append("title")
-      .text(function(d) { return d.id; });
+
+      .text(function(d) {return d.node; });
+
 
   simulation
-      .nodes(data.nodes)
+      .nodes(nodes)
       .on("tick", ticked);
 
   simulation.force("link")
-      .links(data.links);
+      .links(edges);
       
        function ticked() {
+       
     link
         .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
@@ -104,7 +169,79 @@ var simulation = d3.forceSimulation()
         .attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
   }
-});
+  
+  //----------------------------------------------on click node----------------------------------------------
+
+
+    rows_nodes.on("click", clickRowsNode);
+    node.on("click", clickNode);
+    
+        
+    function clickRowsNode(d, i) {
+    if (d3.select(this).classed("rowSelected")) {
+        d3.select(this).classed("rowSelected", false);
+        d3.select("#node" + d.node).classed( "nodeSelected", false);
+        } 
+        else 
+        {
+        d3.select(this).classed("rowSelected", true);
+        d3.select("#node" + d.node).classed( "nodeSelected", true);
+        }
+    }
+    
+    
+    function clickNode(d, i) {
+    
+    if (d3.select(this).classed("nodeSelected")) {
+        d3.select(this).classed("nodeSelected", false);
+        d3.select("#row_node" + d.node).classed("rowSelected",false);
+        } 
+        else 
+        {
+        d3.select(this).classed("nodeSelected", true);
+        d3.select("#row_node" + d.node).classed("rowSelected",true);
+        }
+    }
+    
+    //-----------------------------------------on click edges-----------------------------------
+    
+    rows_links.on("click", clickRowsLink);
+    link.on("click", clickLink);
+    
+    function clickRowsLink(d, i) {
+    if (d3.select(this).classed("rowSelected")) {
+        d3.select(this).classed("rowSelected", false);
+        d3.select("#link" + d.source.node + d.target.node).classed( "linkSelected", false);
+        } 
+        else 
+        {
+        d3.select(this).classed("rowSelected", true);
+        d3.select("#link" + d.source.node + d.target.node).classed( "linkSelected", true);
+        }
+    }
+    
+     function clickLink(d, i) {
+    
+    if (d3.select(this).classed("linkSelected")) {
+        d3.select(this).classed("linkSelected", false);
+        d3.select("#row_link" + d.source.node + d.target.node).classed("rowSelected",false);
+        } 
+        else 
+        {
+        d3.select(this).classed("linkSelected", true);
+        d3.select("#row_link" + d.source.node + d.target.node).classed("rowSelected",true);
+        }
+    }
+    
+    
+   //faire un bouton pour tout deselectionner
+    
+	});//sortie edges
+	});//sortie nodes
+			
+
+
+                
 
 function dragstarted(d) {
   if (!d3.event.active) simulation.alphaTarget(0.3).restart();
@@ -122,12 +259,4 @@ function dragended(d) {
   d.fx = null;
   d.fy = null;
 }
-
-
- /* d3.csv('data.csv', function(data) {
-            var a = function(row) {
-                console.log(Object.keys(row));
-		};
-           
-        });*/
 
